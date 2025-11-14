2646d2a8edafbb8c69e4560ae861e40d3bfc71bf
Kshitij Lauria <kslauria@gmail.com>
2025-11-14 15:25:05 +0530

    script to run some difficult cases

---
+++ run-hard-cases.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+make
+
+for case in 2025-09-15-hard 2025-10-18-hard 2025-10-14-hard 2025-10-20-hard 2025-10-29-hard 2025-10-30-hard 2025-10-05-hard 2025-10-28-hard; do
+  echo $case
+  time bin/mccw -v0 ../pips2mcc/generated-weighted-instances/$case
+  echo
+done
+
